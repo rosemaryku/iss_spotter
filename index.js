270559b11d56282af7dc269d5110fb9f75dab52e
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-20 18:15:26 -0500

    Created nested arrays

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+const {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+} = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -17,17 +22,35 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 //   console.log("It worked! Returned coordinates:", coordinates);
 // });
 
-fetchISSFlyOverTimes(
-  { latitude: "43.6426", longitude: "-79.4002" },
-  (error, data) => {
-    if (error) {
-      console.log(`It didn't work! ${error}`);
-      return;
-    }
+// fetchISSFlyOverTimes(
+//   { latitude: "43.6426", longitude: "-79.4002" },
+//   (error, data) => {
+//     if (error) {
+//       console.log(`It didn't work! ${error}`);
+//       return;
+//     }
 
-    console.log(
-      `It worked! The 5 upcoming times that the ISS will fly over the given coorinates and the durations include the following: `,
-      data
-    );
+//     console.log(
+//       `It worked! The 5 upcoming times that the ISS will fly over the given coorinates and the durations include the following: `,
+//       data
+//     );
+//   }
+// );
+
+// FINAL FUNCTION
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-);
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
--- iss.js
+++ iss.js
@@ -67,4 +67,34 @@ const fetchISSFlyOverTimes = (coordinates, callback) => {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+// NESTED FUNCTION
+// Ochestrates multiple API requests to determine the next 5 upcoming ISS flyovers for the users current location.
+
+const nextISSTimesForMyLocation = function (callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+};
